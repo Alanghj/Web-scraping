c82d68c0a46f4d15feae311879de0775c7ee3254
Alanghj <alancardoso122@hotmail.com>
2021-11-07 21:58:28 -0300

    1.1 Creating bot_search

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from bots.bot_search import bot_search
+
+if __name__ == "__main__":
+        bot_search() 
--- bots/bot_search.py
+++ bots/bot_search.py
@@ -7,16 +7,14 @@ TERM_SEARCH = input('What do you want to search for? ')
 def bot_search():
     wikipedia.set_lang('en')
 
-    wiki_Search = wikipedia.search(TERM_SEARCH, results=4)
+    wiki_Search = wikipedia.search(TERM_SEARCH, results=6)
     print(f'\n{wiki_Search}\n')
 
     wiki_article = wikipedia.summary(TERM_SEARCH, sentences=10)
     print(f'\n{wiki_article}\n')
 
-    with open(f'article/subject.txt', 'w', encoding="utf-8") as f:
+    with open('article/subject.txt', 'w', encoding="utf-8") as f:
         for article in wiki_article:
             f.writelines(article)
 
 
-if __name__ == '__main__':
-    bot_search() 
